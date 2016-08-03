25ba939efb9424f046bac0aa0afa0296fd5ddbfb
andrew-kamenchuk <andrew.kamenchuk@gmail.com>
2016-08-03 05:11:37 +0300

    addhistory hook

--- zsh/runcmd/zshrc.d/history.zsh
+++ zsh/runcmd/zshrc.d/history.zsh
@@ -11,3 +11,8 @@ setopt hist_ignore_all_dups
 setopt hist_ignore_space
 setopt hist_verify
 setopt share_history
+
+# do not save typos in the history
+zshaddhistory() {
+    whence ${${(z)1}[1]} > /dev/null || return 1
+}
