60d8e6478fdaa99a85b85992f7ad6bab24ffc70d
andrew-kamenchuk <andrew.kamenchuk@gmail.com>
2016-08-04 19:20:30 +0300

    better tmux

---
+++ zsh/runcmd/zshrc.d/tmux.zsh
@@ -0,0 +1,10 @@
+if [[ ! -d $HOME/.tmux/plugins/tpm ]]; then
+    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+fi
+
+if [ -z $TMUX ]; then
+    tmux attach -t main || tmux new -s main
+fi
+
+if [ -n $TMUX ]; then
+fi
