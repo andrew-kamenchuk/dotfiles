241cce8c919c1fdfe162af1c994d9b9d88d5bd76
Andrew Kamenchuk <andrew.kamenchuk@gmail.com>
2016-08-04 22:49:44 +0300

    fix

--- zsh/install.zsh
+++ zsh/install.zsh
@@ -1,5 +1,5 @@
 #! /usr/bin/env zsh
-[[ ! -z $ZDOTDIR && ! -d $ZDOTDIR ]] && mkdir $ZDOTDIR
+[[ -n $ZDOTDIR && ! -d $ZDOTDIR ]] && mkdir $ZDOTDIR
 
 for file in ${0:a:h}/runcmd/*(.); do
     ln -s $file ${ZDOTDIR:-$HOME}/.${file:t}
