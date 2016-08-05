ffe122cf274f3cb148fe2f3a9c6a0117b285405b
Andrew Kamenchuk <andrew.kamenchuk@gmail.com>
2016-08-05 03:55:25 +0300

    fix

--- zsh/zshrc.d/tmux.zsh
+++ zsh/zshrc.d/tmux.zsh
@@ -2,10 +2,4 @@ if [[ ! -d $HOME/.tmux/plugins/tpm ]]; then
     git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 fi
 
-if [ -z "$TMUX" ]; then
-#   tmux attach -t main || tmux new -s main
-    tmux new
-fi
-
-if [ -n "$TMUX" ]; then
-fi
+[ -n "$TMUX" ] || tmux new
